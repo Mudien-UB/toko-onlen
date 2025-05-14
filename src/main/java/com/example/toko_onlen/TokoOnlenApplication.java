04ff296e4418b8ba0f66ce68a011a29b5fc3acac
Mudien-UB <mudienub@gmail.com>
2025-05-14 18:44:43 +0700

    init project

---
+++ src/main/java/com/example/toko_onlen/TokoOnlenApplication.java
@@ -0,0 +1,13 @@
+package com.example.toko_onlen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokoOnlenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokoOnlenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/toko_onlen/TokoOnlenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toko_onlen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokoOnlenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
